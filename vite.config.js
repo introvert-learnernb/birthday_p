9a2d2bf6895f2ff559330cd90eac943a0a1e6037
NB <introvertlearnernb@gmail.com>
2024-10-16 18:16:21 +0545

    some url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/birthday_p/', // Adjust this base path for GitHub Pages
+  base: '/', // Adjust this base path for GitHub Pages
   build: {
     rollupOptions: {
       external: ['fsevents'], // Exclude fsevents from the build
